f4b65fe52f0f1219faeea69f8f07d6a08017f9a2
pearl.mari <142984320+MariaVassilikopoulou@users.noreply.github.com>
2024-06-08 16:26:47 +0200

    Update Products.cs

--- API_med_dotNET/Models/Products.cs
+++ API_med_dotNET/Models/Products.cs
@@ -3,8 +3,8 @@
     public class Products
     {
        public int Id { get; set; }
-       public string ProductName { get; set; }
+       public string? ProductName { get; set; }
        public int  ProductPrice { get; set; }
-       public string  ProductType { get; set; }
+       public string?  ProductType { get; set; }
     }
 }
